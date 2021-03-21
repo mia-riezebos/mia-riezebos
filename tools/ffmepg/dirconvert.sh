32e204a4e912bb6d2188e1a7dcd57a10035d7d78
Mia <mia@mia.cx>
2021-03-21 08:51:16 +0100

    numerous fixes
    added a working directory
    fixed documentation to work with line breaks
    save to an output directory in the working directory, rather than outputting to the working directory.

--- tools/ffmepg/dirconvert.sh
+++ tools/ffmepg/dirconvert.sh
@@ -2,15 +2,18 @@
 #
 # Quickly convert all the files of a certain filetype in a directory to another filetype using ffmpeg (useful for say, converting wav to ogg for sharing).
 
-inext="wav"         # input filetype/extension
-outext="ogg"        # output filetype/extension   !!(also change codec)!!
-codec="libvorbis"   # output codec
-bitrate="128k"      # output bitrate
+inext="wav"                                       # input filetype/extension
+outext="ogg"                                      # output filetype/extension   !!(also change codec)!!
+codec="libvorbis"                                 # output codec
+bitrate="128k"                                    # output bitrate
+read -e -p "Working directory: " -i "./" dir      # working directory (where to find the files)
 
+cd $dir
+mkdir ./out
 for file in *\.$inext                             # loop through all the files with the given input filetype/extension
   do 
-    output=$(echo $file | sed 's/.$inext\//g')    # remove the input file extension for output filename
-    ffmpeg -y -i $file\                           # ffmpeg with the current file as input
-      -c:a $codec -b:a $bitrate -vn\              # set audio codec & bitrate
-      $output\.$outext                            # output filename & extension
+    output=$(echo $file | sed "s/.wav//g")        # remove the input file extension for output filename
+    ffmpeg -loglevel error -y -i $file           `# ffmpeg with $file as input`\
+      -c:a $codec -b:a $bitrate -vn              `# set codec and bitrate, ignore video`\
+      ./out/$output\.$outext                     `# set output`
   done
